6d2efbaadf7eec43c488aaa9483c5d0cf43e034a
fantomas08 <44437583+fantomas08@users.noreply.github.com>
2019-02-13 16:54:37 +0100

    Distributor and Zone control

--- eclipse-workspace/Power Plants/src/control/DistributorControl.java
+++ eclipse-workspace/Power Plants/src/control/DistributorControl.java
@@ -1,29 +1,58 @@
 package control;
 
+import java.util.List;
+
+import entities.DistributionLine;
+import entities.DistributionNetwork;
+import entities.Distributor;
+
 public class DistributorControl extends Control {
 
 	//Method to store new distributor at bd
-	public void addDistributor() {
-		
+	public void addDistributor(Distributor distributor) {
+		getOdb().store(distributor);
 	}
 	
-	public void addNetwork() {
-		
+	public void addNetwork(Distributor distributor, DistributionNetwork network) {
+		distributor.getNetworks().add(network);
+		getOdb().store(distributor);
 	}
 	
-	public void addLine() {
-		
+	public void addLine(DistributionLine line, DistributionNetwork network) {
+		network.getLines().add(line);
+		getOdb().store(network);
 	}
 	
-	public void deleteNetwork() {
+	// Hay que probar
+	public void deleteNetwork(int networkIndex, Distributor distributor) {
+		DistributionNetwork network = distributor.getNetworks().get(networkIndex);
 		
+		if (!network.getLines().isEmpty()) {
+			for (DistributionLine line : distributor.getNetworks().get(networkIndex).getLines()) {
+				getOdb().delete(line);
+			}
+		}
+		getOdb().delete(network);
 	}
 	
-	public void deleteLine() {
-		
+	// Hay que probar
+	public void deleteLine(DistributionLine line, DistributionNetwork network) {
+		network.getLines().remove(line);
+		getOdb().delete(line);
 	}
 	
-	public void deleteDistributor() {
-		
+	// Hay que probar
+	public void deleteDistributor(Distributor distributor) {
+		if (!distributor.getNetworks().isEmpty()) {
+			for (DistributionNetwork network : distributor.getNetworks()) {
+				if (!network.getLines().isEmpty()) {
+					for (DistributionLine line : network.getLines()) {
+						getOdb().delete(line);
+					}
+				}
+				getOdb().delete(network);
+			}
+		}
+		getOdb().delete(distributor);
 	}
 }
--- eclipse-workspace/Power Plants/src/control/ServiceZoneControl.java
+++ eclipse-workspace/Power Plants/src/control/ServiceZoneControl.java
@@ -1,26 +1,24 @@
 package control;
 
+import entities.DistributionLine;
 import entities.ServiceZone;
 
 public class ServiceZoneControl extends Control {
-
+	
+	// Method to add new zone or modify existing
 	public void addZone(ServiceZone zone) {
 		getOdb().store(zone);
 	}
 	
-	public void setAvgConsumption(ServiceZone zone) {
-		
+	// Method to add new distribution line to service zone
+	public void registerDistributionLine(ServiceZone zone, DistributionLine line) {
+		line.getZones().add(zone);
+		getOdb().store(line);
 	}
 	
-	public void setNumConsumers(ServiceZone zone) {
-		
-	}
-	
-	public void registerDistributionLine() {
-		
-	}
-	
-	public void deleteDistributionLine() {
-		
+	// Method to remove distribution line from service zone
+	public void deleteDistributionLine(ServiceZone zone, DistributionLine line) {
+		line.getZones().remove(zone);
+		getOdb().store(line);
 	}
 }
